31254778bce02cf696d82ef7c347f5103c495559
root <root@vds2917416.my-ihor.ru>
2026-02-26 10:26:59 +0000

    Интегрировал PWA

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,3 +26,11 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/build/sw.js')
+            .then(reg => console.log('SW registered', reg))
+            .catch(err => console.error('SW registration failed:', err));
+    });
+}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+
     plugins: [
+
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
         }),
+
         vue({
             template: {
                 transformAssetUrls: {
@@ -16,5 +20,65 @@ export default defineConfig({
                 },
             },
         }),
+
+        VitePWA({
+
+            registerType: 'autoUpdate',
+
+            injectRegister: 'auto',
+
+            includeAssets: [
+                'icons/icon-192.png',
+                'icons/icon-512.png'
+            ],
+
+            manifest: {
+
+                name: 'Wix',
+                short_name: 'Wix',
+
+                start_url: '/',
+
+                display: 'standalone',
+
+                background_color: '#0f172a',
+                theme_color: '#0f172a',
+
+                orientation: 'portrait',
+
+                icons: [
+                    {
+                        src: '/icons/wix_logo4.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/wix_logo4.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+
+            workbox: {
+
+                // ВАЖНО: отключаем fallback чтобы не ломать SSR
+                navigateFallback: null,
+
+                // НЕ кешируем страницы
+                runtimeCaching: [],
+
+                // обновление service worker сразу
+                clientsClaim: true,
+                skipWaiting: true
+            },
+
+            devOptions: {
+                enabled: false
+            }
+
+        })
+
     ],
+
 });
